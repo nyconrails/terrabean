31cff936ad1107fc9bed4504579f3d2f13122c1f
David Lesches <david@lesches.com>
2012-08-15 17:56:09 -0400

    Skeleton App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Terrabean::Application.config.secret_token = 'f31d3bd887584a694ab2448a1e3c4362e392bfef9775f67f3486240856b717a8b4e4ef55074fc7dd4d76bc8b699d32f677f00fc9049d0b10866cb0a581878ae0'
